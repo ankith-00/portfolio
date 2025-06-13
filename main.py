216bde2203e493c80aa814cc4084901fccf69073
Ankith Kumar <ankith7800@gmail.com>
2025-06-14 00:12:59 +0530

    added images folder

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 import streamlit as st 
+from pathlib import Path
 
 
-st.header('Hello, World')
+# - - - - - ADDING CSS FILE - - - - - - - - - - - - - - - - - - - - - - -  -> 
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "main.css"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# - - - - - - APPLYING CSS - - - - - - - - - ->
+with open(css_file) as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
